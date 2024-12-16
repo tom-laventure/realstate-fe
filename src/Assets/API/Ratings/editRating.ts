d272e06a77ef9347a69233b2621fde4ec4bb178f
Tom <tom.laventure42@gmail.com>
2024-12-15 21:21:25 -0800

    estate ratings update

--- src/Assets/API/Ratings/editRating.ts
+++ src/Assets/API/Ratings/editRating.ts
@@ -1,13 +1,6 @@
 import instance from "Assets/Axios/axios";
-import rating from "Assets/Types/EstateRatingType";
+import rating, { RatingResponse } from "Assets/Types/EstateRatingType";
 
-interface EditRatingResponse {
-    estate_ratings: rating[],
-    user_rating: rating
-}
+const editRating = (body: rating) => instance.patch<RatingResponse>(`v1/estate_ratings/${body.id}`, body)
 
-
-const editRating = (body: rating) => instance.patch<EditRatingResponse>(`v1/estate_ratings/${body.id}`, body)
-
-export { EditRatingResponse }
 export default editRating
--- src/Assets/API/Ratings/postRating.ts
+++ src/Assets/API/Ratings/postRating.ts
@@ -1,12 +1,7 @@
 import instance from "Assets/Axios/axios";
-import rating from "Assets/Types/EstateRatingType";
-
-interface PostRatingResponse {
-    estate_ratings: rating[],
-    user_rating: rating
-}
+import rating, { RatingResponse } from "Assets/Types/EstateRatingType";
 
 
-const postRating = (body: rating) => instance.post<PostRatingResponse>(`v1/estate_ratings`, body)
+const postRating = (body: rating) => instance.post<RatingResponse>(`v1/estate_ratings`, body)
 
 export default postRating
