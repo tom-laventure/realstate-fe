84f0bb8acf4881716111cbcd11b62bf78fcecb7b
Tom <tom.laventure42@gmail.com>
2025-11-30 21:06:57 -0800

    theme update

--- src/index.js
+++ src/index.js
@@ -6,17 +6,30 @@ import router from './Router/main'
 import StoreProvider from './Store/StoreProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    }
+  }
+})
 
 const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<QueryClientProvider client={queryClient}>
-		<StoreProvider>
-			<>
-				<RouterProvider router={router} />
-				<ReactQueryDevtools initialIsOpen={false} />
-			</>
-		</StoreProvider>
-	</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+        <StoreProvider>
+            <ThemeProvider theme={theme}>
+                <RouterProvider router={router} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </ThemeProvider>
+        </StoreProvider>
+    </QueryClientProvider>
 )
